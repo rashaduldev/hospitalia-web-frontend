f158f9d9a9cd6b7ef98f5dfc64e15a1fa35f1e8a
Md Rashadul Islam <rashadul.dev@gmail.com>
2026-02-05 16:28:28 +0600

    feat: doctor login page design done

--- app/[locale]/(auth)/doctor/login/page.tsx
+++ app/[locale]/(auth)/doctor/login/page.tsx
@@ -1,11 +1,70 @@
-import React from 'react'
+"use client";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Header from "@/components/pages/home/Header";
+import { LoginformSchema, LoginFormValues } from "@/schema/ueser.schema";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
 
-const DoctorRLoginPage = () => {
+export default function DoctorRLoginPage() {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginFormValues>({
+    resolver: zodResolver(LoginformSchema),
+  });
+
+  const onSubmit = async (data: LoginFormValues) => {
+    console.log("LOGIN FORM DATA", data);
+  };
+
   return (
     <div>
-      DoctorRLoginPage
-    </div>
-  )
-}
+      <Header />
+      <div className="min-h-[80vh] sm:min-h-screen px-4 flex items-center">
+        <div className="max-w-111.5 mx-auto w-full">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <h3 className="text-xl leading-7"> Login as a Provider</h3>
+              <p className="mb-8 text-sm">Enter your details below to login</p>
 
-export default DoctorRLoginPage
+              <div className="space-y-4">
+                {/* Phone */}
+                <div className="space-y-1">
+                  <Label>Phone</Label>
+                  <Input type="number" {...register("phone")} placeholder="08012345678" />
+                  <p className="text-xs text-destructive">{errors.phone?.message}</p>
+                </div>
+
+                {/* Password */}
+                <div className="space-y-1">
+                  <Label>Password</Label>
+                  <Input type="password" {...register("password")} placeholder="••••••••" />
+                  <p className="text-xs text-destructive">{errors.password?.message}</p>
+                </div>
+              </div>
+                    {/* Submit */}
+              <div className="mt-6 w-full text-left">
+                <Button className="w-full" type="submit" disabled={isSubmitting}>
+                  {isSubmitting ? "Login..." : "Login"}
+                </Button>
+              </div>
+             <div className="mt-8 flex flex-col">
+               <Link href="/doctor/registration" className="text-sm font-medium text-secondary hover:underline mb-4">
+              Don't have an account? Sign up
+              </Link>
+              <Link href="/doctor/forgot-password" className="text-sm font-medium text-primary hover:underline">
+              Forgot your password?
+              </Link>
+             </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
--- schema/ueser.schema.ts
+++ schema/ueser.schema.ts
@@ -1,5 +1,19 @@
 import { z } from "zod";
 
+
+// User Login schema
+export const LoginformSchema = z
+  .object({
+    phone: z
+      .string()
+      .min(10, "Phone number is too short")
+      .max(15, "Phone number is too long"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+  })
+
+export type LoginFormValues = z.infer<typeof LoginformSchema>
+
+// Doctor Registration schema
 export const formSchema = z
   .object({
     name: z.string().min(2, "Name is required"),
