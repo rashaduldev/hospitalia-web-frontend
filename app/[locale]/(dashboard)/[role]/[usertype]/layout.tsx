9ea8a4603bf8fa4aeea6c99f71ef64c5c7b18b3d
Md Rashadul Islam <rashadul.dev@gmail.com>
2026-02-17 13:02:50 +0600

    fix: nextconfig update and dashboard layout import fix

--- app/[locale]/(dashboard)/[role]/[usertype]/layout.tsx
+++ app/[locale]/(dashboard)/[role]/[usertype]/layout.tsx
@@ -1,20 +1,20 @@
-import { cookies } from "next/headers";
 import { getCurrentUser } from "@/actions/user.actions";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import { SiteHeader } from "@/components/site-header";
+import ErrorHandle from "@/components/common/ErrorHandle";
 
 export default async function UserLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const res = await getCurrentUser();  
+  const res = await getCurrentUser();
   if (!res) {
     return (
       <ErrorHandle
-        message={res?.message || "Unauthorized access. Please log in."} 
+        message={res?.message || "Unauthorized access. Please log in."}
         status={res?.statusCode}
       />
     );
@@ -23,32 +23,27 @@ export default async function UserLayout({
 
   return (
     <div>
-      {/* <DashboardNavbar user={user} /> */}
       <TooltipProvider>
-           <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
-          "--header-height": "calc(var(--spacing) * 12)",
-        } as React.CSSProperties
-      }
-    >
-      <AppSidebar variant="inset" user={user} />
-      <SidebarInset>
-        <SiteHeader />
-        <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              {/* <SectionCards /> */}
-              {/* <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
-              </div> */}
-             {children}
+        <SidebarProvider
+          style={
+            {
+              "--sidebar-width": "calc(var(--spacing) * 72)",
+              "--header-height": "calc(var(--spacing) * 12)",
+            } as React.CSSProperties
+          }
+        >
+          <AppSidebar variant="inset" user={user} />
+          <SidebarInset>
+            <SiteHeader />
+            <div className="flex flex-1 flex-col">
+              <div className="@container/main flex flex-1 flex-col gap-2">
+                <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                  {children}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+          </SidebarInset>
+        </SidebarProvider>
       </TooltipProvider>
     </div>
   );
