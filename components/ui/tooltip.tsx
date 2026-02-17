12026c756106f26613f1d4fa63976be4fc5fc6ae
Md Rashadul Islam <rashadul.dev@gmail.com>
2026-02-17 12:35:12 +0600

    fix: tailwind some class update

--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -48,7 +48,7 @@ function TooltipContent({
         {...props}
       >
         {children}
-        <TooltipPrimitive.Arrow className="size-2.5 rotate-45 rounded-[2px] bg-foreground fill-foreground z-50 translate-y-[calc(-50%_-_2px)]" />
+        <TooltipPrimitive.Arrow className="size-2.5 rotate-45 rounded-[2px] bg-foreground fill-foreground z-50 translate-y-[calc(-50%-2px)]" />
       </TooltipPrimitive.Content>
     </TooltipPrimitive.Portal>
   )
