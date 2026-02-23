c02e049e9614c07e63bc0c97717301797284af95
Md Rashadul Islam <rashadul.dev@gmail.com>
2026-02-23 09:57:30 +0600

    fix: remove the locale dependency, Use the cn function

--- app/[locale]/(auth)/login/page.tsx
+++ app/[locale]/(auth)/login/page.tsx
@@ -3,10 +3,11 @@ import LoginForm from "@/components/pages/login/LoginForm";
 import { Metadata } from "next";
 import Image from "next/image";
 import patientLoginImg from "../../../../public/assets/patient-login.png";
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "Hospitalia | Login",
-  description:"Login to your Hospitalia account to securely access hospital services, manage appointments, and connect with healthcare professionals.",
+  description: "Login to your Hospitalia account to securely access hospital services, manage appointments, and connect with healthcare professionals.",
 };
 
 export default async function LoginPage({
@@ -21,9 +22,10 @@ export default async function LoginPage({
     <div className="flex flex-col min-h-screen">
       <Header />
       <main
-        className={`flex-1 flex flex-col ${
-          isPatient ? "md:grid md:grid-cols-2" : "items-center justify-center"
-        } gap-0`}
+        className={cn(
+          "flex-1 flex flex-col gap-0",
+          isPatient ? "md:grid md:grid-cols-2" : "items-center justify-center",
+        )}
       >
         {isPatient && (
           <div className="relative w-full h-75 md:h-full bg-muted">
@@ -38,12 +40,12 @@ export default async function LoginPage({
         )}
 
         <div
-          className={`w-full flex items-center justify-center py-10
-          ${
+          className={cn(
+            "w-full flex items-center justify-center py-10",
             isPatient
               ? "md:justify-start md:pl-20 px-5 md:px-0"
-              : "mx-auto min-h-[60vh] px-5 md:px-0"
-          }`}
+              : "mx-auto min-h-[60vh] px-5 md:px-0",
+          )}
         >
           <div className="w-full max-w-112.5">
             <LoginForm isPatient={isPatient} />
