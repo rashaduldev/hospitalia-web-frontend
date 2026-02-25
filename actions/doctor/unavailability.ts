8cc8b1690b36381c62329a6bcc6613995844aacf
Md Rashadul Islam <rashadul.dev@gmail.com>
2026-02-25 13:00:30 +0600

    fix: Doctor unavailability api

---
+++ actions/doctor/unavailability.ts
@@ -0,0 +1,60 @@
+"use server";
+
+import { apiClient } from "@/lib/api";
+import { getAccessToken } from "../auth";
+
+// Get all weekly unavailability
+export const getDoctorUnAvailability = async ({
+  lang,
+  doctorUserId,
+}: {
+  lang: string;
+  doctorUserId: number;
+}) => {
+  const token = await getAccessToken();
+
+  const res = await apiClient({
+    endpoint: `/api/doctors/unavailability/all/doctorUserId/${doctorUserId}`,
+    method: "GET",
+    params: { lang },
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res;
+};
+
+// doctors weekly unavailability schedule set
+export const createDoctorUnAvailability = async ({
+  lang,
+  doctorUserId,
+  unavailableDate,
+}: {
+  lang: string;
+  doctorUserId: number;
+  unavailableDate: string;
+}) => {
+  const token = await getAccessToken();
+
+  const res = await apiClient({
+    endpoint: "/api/doctors/unavailability/set",
+    method: "POST",
+    body: {
+      doctorUserId,
+      unavailableDate,
+    },
+    params: { lang },
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res;
+};
+
+// Delete an unavailability slot
+export const deleteAvailabilitySlot = async ({ id }: { id: number }) => {
+  const token = await getAccessToken();
+
+  const res = await apiClient({
+    endpoint: `/api/doctors/unavailability/${id}`,
+    method: "DELETE",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res;
+};
